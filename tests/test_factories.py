616b927ea06792974d64608c8fb631d3cecf52e1
Pelidyai <dima-pile@mail.ru>
2023-05-07 14:30:11 +0700

    Create new API methods in temp db

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -6,11 +6,11 @@ from jarvis_factory.factories.jcalc import JCalcClassesFactory
 
 
 class FactoriesTest(unittest.TestCase):
-    def test_singleton_db_controller(self):
+    def test_not_singleton_db_controller(self):
         controller1: DBController = JCalcClassesFactory.create_db_controller()
         controller2: DBController = JCalcClassesFactory.create_db_controller()
 
-        self.assertEqual(controller1, controller2)
+        self.assertNotEqual(controller1, controller2)
 
 
 if __name__ == '__main__':
