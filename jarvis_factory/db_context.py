dc238591ec4dae7ba64d25086cdc687b76c4f5dc
Pelidyai <dima-pile@mail.ru>
2023-06-13 21:44:06 +0700

    Global deps update. Adapt to real user info db interactors.

--- jarvis_factory/db_context.py
+++ jarvis_factory/db_context.py
@@ -1,3 +1,4 @@
+from jarvis_db.db_config import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -10,5 +11,5 @@ class DbContext:
             logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
         engine = create_engine(connection_sting)
         session = sessionmaker(bind=engine, autoflush=False)
-        # Base.metadata.create_all(engine)
+        Base.metadata.create_all(engine)
         self.session = session
--- jarvis_factory/factories/jdb.py
+++ jarvis_factory/factories/jdb.py
@@ -1,11 +1,14 @@
 from jarvis_db.repositores.mappers.market.person import AccountTableToJormMapper, UserTableToJormMapper
+from jarvis_db.repositores.mappers.market.person.token_mappers import TokenTableMapper
 from jarvis_db.repositores.market.person import AccountRepository, UserRepository
+from jarvis_db.repositores.market.person.token_repository import TokenRepository
+from jarvis_db.services.market.person import TokenService
 from jarvis_db.services.market.person.account_service import AccountService
 from jarvis_db.services.market.person.user_service import UserService
 
 from jorm.jarvis.db_access import JORMCollector, UserInfoCollector
-
-from jarvis_factory.temp.temp_db import TempUserInfoCollector, TempJORMCollector
+from jarvis_db.access.user_info_collector_impl import UserInfoCollectorImpl
+from jarvis_factory.temp.temp_db import TempJORMCollector
 
 
 class JDBClassesFactory:
@@ -13,7 +16,8 @@ class JDBClassesFactory:
     def create_user_info_collector(session) -> UserInfoCollector:
         account_service = AccountService(AccountRepository(session), AccountTableToJormMapper())
         user_service = UserService(UserRepository(session), UserTableToJormMapper())
-        return TempUserInfoCollector(user_service, account_service)
+        token_service = TokenService(TokenRepository(session), TokenTableMapper())
+        return UserInfoCollectorImpl(account_service, user_service, token_service)
 
     @staticmethod
     def create_jorm_collector() -> JORMCollector:
