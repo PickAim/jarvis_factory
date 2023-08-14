7216c85b3cb1fe9d17fbb09b680c1d95f3688480
Pelidyai <dima-pile@mail.ru>
2023-08-15 00:00:51 +0700

    Return created MP ids

--- jarvis_factory/startup.py
+++ jarvis_factory/startup.py
@@ -5,9 +5,13 @@ from jarvis_factory.support.constants import SUPPORTED_MARKETPLACES
 from jarvis_factory.support.jdb.services import JDBServiceFactory
 
 
-def init_supported_marketplaces(session: Session):
+def init_supported_marketplaces(session: Session) -> list[int]:
     marketplace_service = JDBServiceFactory.create_marketplace_service(session)
+    marketplace_ids: list[int] = []
     for marketplace_name in SUPPORTED_MARKETPLACES:
         if marketplace_service.find_by_name(marketplace_name) is None:
             marketplace = Marketplace(marketplace_name)
             marketplace_service.create(marketplace)
+        found_info: tuple[Marketplace, int] = marketplace_service.find_by_name(marketplace_name)
+        marketplace_ids.append(found_info[1])
+    return marketplace_ids
